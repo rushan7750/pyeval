ae88a73b6d60ba0116d2cc9da554a60ba77c70b8
rushan7750 <62587142+rushan7750@users.noreply.github.com>
2020-11-27 19:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-@app.route("/sms", methods=['POST'])
+@app.route("/", methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
@@ -16,8 +12,6 @@ def sms_reply():
     # Create reply
     resp = MessagingResponse()
     resp.message("You said: {}".format(msg))
-    resp.message(str(eval(msg)))
-
     return str(resp)
 
 if __name__ == "__main__":
